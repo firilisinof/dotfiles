4f777eee9bdcbe2ee8ee2b819dd73522534dcb92
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2026-03-21 21:12:51 +0100

    feat(home): configure git with home manager

--- modules/home/lucas/git/default.nix
+++ modules/home/lucas/git/default.nix
@@ -1,7 +1,28 @@
 { ... }:
 
 {
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+  };
+
   programs.git = {
     enable = true;
+    lfs.enable = true;
+    settings = {
+      alias = {
+        co = "checkout";
+        st = "status --short --branch";
+        last = "log -1 --stat";
+      };
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+      rebase.autoStash = true;
+      user = {
+        name = "Lucas de Sousa Rosa";
+        email = "roses.lucas404@gmail.com";
+      };
+    };
   };
 }
