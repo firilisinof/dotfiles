1b09fef97c1e0223b8b2134786ca954e87b1d5fe
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2026-03-22 19:18:58 +0100

    feat(home): configure zsh aliases, dotDir, and keybindings

--- modules/home/lucas/zsh/default.nix
+++ modules/home/lucas/zsh/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
 {
   programs.starship = {
@@ -8,9 +8,30 @@
 
   programs.zsh = {
     enable = true;
+    
+    dotDir = "${config.xdg.configHome}/zsh";
+
+    shellAliases = lib.mkMerge [
+      (lib.mkIf config.programs.bat.enable { cat = "bat --plain"; })
+      (lib.mkIf config.programs.lsd.enable {
+        tree = "lsd --tree";
+      })
+      rec {
+        nano = "nvim";
+        vi = "nvim";
+        vim = "nvim";
+        vimdiff = "nvim -d";
+      }
+    ];
+
+    envExtra = ''
+      export SHELL=$(which zsh)
+    '';
+
     initContent = ''
       source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
       source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+      bindkey '^W' backward-kill-word
     '';
   };
 }
