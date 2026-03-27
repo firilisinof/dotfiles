7f1fda2a8d1d4a96deb30ac4924f74ff4bfc78a1
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2026-03-27 13:29:23 +0100

    feat(home): add nodejs and move global directory to user space

--- modules/home/lucas/environment.nix
+++ modules/home/lucas/environment.nix
@@ -1,7 +1,12 @@
 { config, ... }:
 
 {
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.npm-global/bin"
+  ];
+
   home.sessionVariables = {
+    NPM_CONFIG_PREFIX = "${config.home.homeDirectory}/.npm-global";
     SSH_AUTH_SOCK = "${config.home.homeDirectory}/.bitwarden-ssh-agent.sock";
   };
 }
--- modules/home/lucas/packages.nix
+++ modules/home/lucas/packages.nix
@@ -8,5 +8,6 @@
     codex
     nixfmt
     ripgrep
+    nodejs
   ];
 }
