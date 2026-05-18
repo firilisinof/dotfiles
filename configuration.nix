a4a95e4d9c850c0a05bccc4a116a766121f8c5ee
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2026-05-18 21:50:01 +0200

    feat: add nix-darwin files

---
+++ configuration.nix
@@ -0,0 +1,86 @@
+{ pkgs, lib, inputs, ... }:
+{
+  environment.etc."sudoers.d/10-passwordless".text = ''
+    lucas ALL=(ALL:ALL) NOPASSWD:SETENV: ALL
+  '';
+
+  fonts.packages = [
+    pkgs.roboto
+    pkgs.nerd-fonts.roboto-mono
+  ];
+
+  nix = {
+    settings.experimental-features = "nix-command flakes";
+  };
+
+  nixpkgs = {
+    hostPlatform = "aarch64-darwin";
+    config.allowUnfree = true;
+  };
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  system = {
+    primaryUser = "lucas";
+    startup.chime = false;
+    defaults = {
+      dock = {
+        mru-spaces = false;
+        autohide = true;
+        orientation = "bottom";
+        tilesize = 64;
+        show-recents = false;
+        minimize-to-application = false;
+        expose-animation-duration = 0.15;
+        showhidden = true;
+        persistent-apps = [];
+        wvous-bl-corner = 1;
+        wvous-br-corner = 1;
+        wvous-tl-corner = 1;
+        wvous-tr-corner = 1;
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        ShowPathbar = true;
+        ShowStatusBar = false;
+        FXPreferredViewStyle = "Nlsv";
+      };
+      spaces = {
+        spans-displays = false;
+      };
+      WindowManager = {
+        StandardHideDesktopIcons = false;
+        EnableStandardClickToShowDesktop = false;
+      };
+      NSGlobalDomain = {
+        AppleInterfaceStyle = null;
+        ApplePressAndHoldEnabled = false;
+        AppleSpacesSwitchOnActivate = true; 
+        AppleShowAllExtensions = true;
+        AppleWindowTabbingMode = "manual";
+        InitialKeyRepeat = 20;
+        KeyRepeat = 2;
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
+        NSAutomaticSpellingCorrectionEnabled = false;
+        NSAutomaticInlinePredictionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticWindowAnimationsEnabled = false;
+        NSDocumentSaveNewDocumentsToCloud = false;
+        NSNavPanelExpandedStateForSaveMode = true;
+        NSWindowShouldDragOnGesture = true;
+        PMPrintingExpandedStateForPrint = true;
+      };
+      trackpad = {
+        TrackpadRightClick = true;
+        TrackpadThreeFingerDrag = false;
+        Clicking = true;
+      };
+    };
+    configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+    stateVersion = 6;
+  };
+
+  users.users.lucas.home = "/Users/lucas";
+}
--- flake.nix
+++ flake.nix
@@ -3,60 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
-    aerospace = {
-      url = "github:nikitabobko/homebrew-tap";
-      flake = false;
-    };
-    janky-borders = {
-      url = "github:FelixKratz/homebrew-formulae";
-      flake = false;
-    };
-    swipeaerospace = {
-      url = "github:mediosz/homebrew-tap";
-      flake = false;
-    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nix-homebrew, home-manager, ... }:
-  let
-    system = "aarch64-darwin";
-  in
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Lucass-MacBook-Pro
-    darwinConfigurations = {
-      "Lucass-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-        inherit system;
-        specialArgs = {
-          inherit self;
-          inherit (inputs) homebrew-core homebrew-cask aerospace janky-borders swipeaerospace;
-        };
-        modules = [
-          ./modules/darwin
-          home-manager.darwinModules.home-manager
-          nix-homebrew.darwinModules.nix-homebrew
-          # Align homebrew taps config with nix-homebrew.
-          ({ config, ... }: {
-            homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
-          })
-        ];
-      };
+    # $ darwin-rebuild build --flake .#birkin
+    darwinConfigurations."birkin" = nix-darwin.lib.darwinSystem {
+      modules = [ ./configuration.nix ];
+      specialArgs = { inherit inputs; };
     };
   };
 }
